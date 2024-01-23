742600618a1cf268021badeb8688ca9cae374964
ducthangchin <trinhducthang201@gmail.com>
2024-01-23 03:24:05 +0300

    update

--- app/client/src/api/serverAPI/apiConfig.js
+++ app/client/src/api/serverAPI/apiConfig.js
@@ -1,7 +1,7 @@
 const apiConfig = {
     imgURL: (path) => `${process.env.REACT_APP_BASE_IMAGE_URL}/${path}`,
     // baseURL: 'http://api.v1.ducthangchin.engineer'
-    baseURL: process.env.REACT_APP_BASE_URL
+    baseURL: process.env.REACT_APP_BASE_API_URL
 }
 
 export default apiConfig
