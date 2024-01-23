3c33259d2ac8ecaeaf4579716c70a59bb2d674bd
trinhducthang201@gmail.com <trinhducthang201@gmail.com>
2024-01-23 23:35:18 +0300

    change env var

--- app/client/src/api/tmdbConfig.js
+++ app/client/src/api/tmdbConfig.js
@@ -1,5 +1,5 @@
 export const config = {
-    baseUrl: `${process.env.REACT_APP_BASE_URL}/tmdb`,
+    baseUrl: `${process.env.REACT_APP_BASE_API_URL}/tmdb`,
     originalImage: imgPath => `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image: imgPath => `https://image.tmdb.org/t/p/w500/${imgPath}`
 }
